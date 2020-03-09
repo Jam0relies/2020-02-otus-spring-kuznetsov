ff5346406c9e379af1b91b5157b4ce9284889d2d
alexander <toboe0017@gmail.com>
2020-03-09 12:51:26 +0400

    Add empty context initialisation

---
+++ homework-01/src/main/java/ru/otus/Main.java
@@ -0,0 +1,9 @@
+package ru.otus;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+    }
+}
