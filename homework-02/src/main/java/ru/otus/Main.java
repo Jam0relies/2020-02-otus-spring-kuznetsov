c45b3ba88758c6b569a4070b8889d05ff84dbb21
alexander <toboe0017@gmail.com>
2020-03-14 16:28:02 +0400

    Add localized question source configuration

--- homework-02/src/main/java/ru/otus/Main.java
+++ homework-02/src/main/java/ru/otus/Main.java
@@ -3,12 +3,10 @@ package ru.otus;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import ru.otus.service.QuizService;
 
 @Configuration
 @ComponentScan
-@PropertySource("classpath:i18n/application_en.properties")
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
--- homework-02/src/main/java/ru/otus/config/I18nConfig.java
+++ homework-02/src/main/java/ru/otus/config/I18nConfig.java
@@ -1,9 +1,13 @@
 package ru.otus.config;
 
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import ru.otus.dao.QuestionDao;
+import ru.otus.dao.QuestionDaoCsvResource;
 
 @Configuration
 public class I18nConfig {
@@ -13,5 +17,11 @@ public class I18nConfig {
         ms.setBasename("/i18n/application");
         ms.setDefaultEncoding("UTF-8");
         return ms;
+    }
+
+    @Bean
+    QuestionDao localizedQuestionsDao(MessageSource messageSource, CsvMapper mapper) {
+        String questionResource = messageSource.getMessage("questions.source.csv", new String[0], LocaleContextHolder.getLocale());
+        return new QuestionDaoCsvResource(questionResource, mapper);
     }
 }
