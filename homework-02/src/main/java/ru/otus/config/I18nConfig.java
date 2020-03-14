24dcd9db8bfa896515634e38b7215e37e532366f
alexander <toboe0017@gmail.com>
2020-03-14 19:43:25 +0400

    Rename localization files

--- homework-02/src/main/java/ru/otus/config/I18nConfig.java
+++ homework-02/src/main/java/ru/otus/config/I18nConfig.java
@@ -16,7 +16,7 @@ public class I18nConfig {
     @Bean
     MessageSource messageSource() {
         ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
-        ms.setBasename("/i18n/application");
+        ms.setBasename("/i18n/local");
         ms.setDefaultEncoding("UTF-8");
         return ms;
     }
