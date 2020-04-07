87547cbcab16599224ca314ca457198cb48d0a24
alexander <toboe0017@gmail.com>
2020-04-07 22:19:17 +0400

    Initialise spring-boot project

---
+++ homework-03/src/test/java/ru/otus/homework03/Homework03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.homework03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
