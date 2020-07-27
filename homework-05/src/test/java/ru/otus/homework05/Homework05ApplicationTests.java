c9332c2cf81810967010888269e4fac951e0c93e
alexander <toboe0017@gmail.com>
2020-07-27 23:26:32 +0400

    Initiate project

---
+++ homework-05/src/test/java/ru/otus/homework05/Homework05ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.homework05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
