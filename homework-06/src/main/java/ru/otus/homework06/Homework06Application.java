6a5863b1c7301d29c3bf65bcdc33ae8743fc0bb4
alexander <toboe0017@gmail.com>
2020-08-03 00:03:41 +0400

    Rename project

--- homework-06/src/main/java/ru/otus/homework06/Homework06Application.java
+++ homework-06/src/main/java/ru/otus/homework06/Homework06Application.java
@@ -1,13 +1,13 @@
-package ru.otus.homework05;
+package ru.otus.homework06;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Homework05Application {
+public class Homework06Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Homework05Application.class, args);
+        SpringApplication.run(Homework06Application.class, args);
     }
 
 }
