89f797488c815066aad49486a5ca9d77622471ff
alexander <toboe0017@gmail.com>
2020-08-19 21:35:01 +0400

    Copy homework-07 to homework-09

---
+++ homework-09/src/test/java/ru/otus/homework07/Homework07ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.homework07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
