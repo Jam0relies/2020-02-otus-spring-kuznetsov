b609d19ffab1ee4922abf768988a359e695aa809
alexander <toboe0017@gmail.com>
2020-08-24 23:23:23 +0400

    Implement Book add/remove logic on Vue

--- homework-10/src/main/java/ru/otus/homework10/rest/BookController.java
+++ homework-10/src/main/java/ru/otus/homework10/rest/BookController.java
@@ -1,12 +1,16 @@
 package ru.otus.homework10.rest;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.otus.homework10.domain.Book;
+import ru.otus.homework10.rest.dto.AddBookRequestDto;
+import ru.otus.homework10.rest.dto.BookDto;
 import ru.otus.homework10.service.BookService;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class BookController {
@@ -19,7 +23,19 @@ public class BookController {
     }
 
     @GetMapping("/api/books")
-    public List<Book> getAllBooks() {
-        return null;
+    public List<BookDto> getAllBooks() {
+        return bookService.getAll().stream().map(b -> modelMapper.map(b, BookDto.class)).collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/api/books/{bookId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void delete(@PathVariable long bookId) {
+        bookService.delete(bookId);
+    }
+
+    @PostMapping("/api/books")
+    public BookDto addAuthor(@RequestBody @Valid AddBookRequestDto bookToAdd) {
+        Book genre = bookService.addBook(bookToAdd.getName());
+        return modelMapper.map(genre, BookDto.class);
     }
 }
