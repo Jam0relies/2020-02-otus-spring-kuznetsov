d0d210287cd8fcf5cb00e12d9b80851f2819ea77
alexander <toboe0017@gmail.com>
2020-08-30 01:38:39 +0400

    Add routing

--- homework-11/frontend/src/main.js
+++ homework-11/frontend/src/main.js
@@ -1,33 +1,36 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 // import Meta from 'vue-meta'
 import App from './App.vue'
+import Home from "./components/Home";
 
-// Vue.use(Router)
-// // Vue.use(Meta)
-//
-// const router = new Router({
-//   routes: [
-//     // {
-//     //   path: '/',
-//     //   name:'home',
-//     //   // component: Authors,
-//     // },
-//     {
-//       path: '/authors',
-//       name: 'authors',
-//       component: Authors
-//     }
-//     // {
-//     //   path: '/post/:id',
-//     //   name:'post',
-//     //   component: Post,
-//     //   props: true,
-//     // },
-//   ],
-//   mode: 'history'
-// })
+Vue.use(Router)
+// Vue.use(Meta)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    // {
+    //   path: '/authors',
+    //   name: 'authors',
+    //   component: Authors
+    // }
+    // {
+    //   path: '/post/:id',
+    //   name:'post',
+    //   component: Post,
+    //   props: true,
+    // },
+  ],
+  mode: 'history'
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
