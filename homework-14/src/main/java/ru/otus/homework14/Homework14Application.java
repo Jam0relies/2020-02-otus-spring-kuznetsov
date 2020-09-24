4cf39b5e7765d431a316177b490af7d2590b82a1
alexander <toboe0017@gmail.com>
2020-09-24 23:42:25 +0400

    Create empty spring batch project

---
+++ homework-14/src/main/java/ru/otus/homework14/Homework14Application.java
@@ -0,0 +1,13 @@
+package ru.otus.homework14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework14Application.class, args);
+	}
+
+}
